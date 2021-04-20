45136968751d36063e7c06eeb706a0f28beb5934
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-20 22:31:40 +0530

    Add a Function to Calculate Employee Wage till a condition of total working hours or days s reached for a Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,25 +3,29 @@ echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1;
 isFullTime=2;
-totalsalary=0;
+Max_Hrs_in_Month=100;
 emprateperHr=20;
 numWorkingDays=20;
-for (( day =1; day<=$numWorkingDays; day++ ))
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHr -lt $Max_Hrs_in_Month &&
+         $totalWorkingDays -lt $numWorkingDays ]]
 do
+        ((totalWorkingDays++))
+        empcheck=$((RANDOM%3));
+        case $empcheck in
+                $isFullTime)
+                        empHrs=8;;
+                $isPartTime)
+                        empHrs=4;;
+                *)
+                        empHrs=0;;
+         esac
+	totalEmpHr=$(($totalEmpHr+$empHrs));
 
-        randomcheck=$((RANDOM%3));
-                case $randomcheck in
-                        $isFullTime)
-                                empHrs=8;;
-                        $isPartTime)
-                                empHrs=4;;
-                        *)
-                                empHrs=0;;
-                esac
-                salary=$(($empHrs*$emprateperHr));
-                totalsalary=$(($totalsalary+$salary));
 done
-echo $salary
+totalsalary=$(($totalEmpHr*$emprateperHr));
 echo $totalsalary
-
 
