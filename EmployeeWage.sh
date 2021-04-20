a2db1509510c885c2f6a23c5f9d1568c73454021
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-20 21:30:14 +0530

    Add a Function to Calculate Part Time Employee Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1
-randomcheck=$((RANDOM%2))
-if [ $isPresent -eq $randomcheck ]
+
+isPartTime=1;
+isFullTime=2;
+emprateperHr=20;
+randomcheck=$((RANDOM%3))
+if [ $isFullTime -eq $randomcheck ]
 then
-        emprateperHr=20
-        empHrs=8
-        salary=$(($empHrs*$emprateperHr))
+        empHrs=8;
+elif [ $isPartTime -eq $randomcheck ]
+then
+        empHrs=4;
 else
-        salary=0
+        empHrs=0;
 fi
+salary=$(($empHrs*$emprateperHr))
+echo $salary
 
