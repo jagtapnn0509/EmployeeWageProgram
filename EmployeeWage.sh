9e3beb2eff4fb517b83848e8d8b44ed468d9cbaa
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-20 21:56:53 +0530

    Add a Function to Calculate Employee Wage for a Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,21 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
-
 
 isPartTime=1;
 isFullTime=2;
+totalsalary=0;
 emprateperHr=20;
-randomcheck=$((RANDOM%3))
-case $randomcheck in
-        $isFullTime)
-                empHrs=8;;
-        $isPartTime)
-                empHrs=4;;
-        *)
-                empHrs=0;;
-esac
+numWorkingDays=20;
+for (( day =1; day<=$numWorkingDays; day++ ))
+do
 
-salary=$(($empHrs*$emprateperHr))
+        randomcheck=$((RANDOM%3));
+                case $randomcheck in
+                        $isFullTime)
+                                empHrs=8;;
+                        $isPartTime)
+                                empHrs=4;;
+                        *)
+                                empHrs=0;;
+                esac
+                salary=$(($empHrs*$emprateperHr));
+                totalsalary=$(($totalsalary+$salary));
+done
 echo $salary
+echo $totalsalary
 
 
